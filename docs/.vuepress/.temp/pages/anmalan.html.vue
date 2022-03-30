980c97dd6e9d527918ba57cad0bcdeb666e49340
Per Åkerberg <per.akerberg@live.se>
2022-03-30 15:57:51 +0200

    Begränsat antal platser

--- docs/.vuepress/.temp/pages/anmalan.html.vue
+++ docs/.vuepress/.temp/pages/anmalan.html.vue
@@ -1,6 +1,7 @@
 <template><h1 id="anmalan" tabindex="-1"><a class="header-anchor" href="#anmalan" aria-hidden="true">#</a> Anmälan</h1>
 <p>Nu är anmälan till fotbollsskolan öppen!
 Registeringen sker via Svenska fotbollsförbundets hemsida och är enkel att gå igenom.
+Observera att det finns ett begränsat antal platser, så anmäl dig redan idag!
 Om du har några frågor eller funderingar, <a href="kontakt">kontakta oss</a>!</p>
 <h2 id="lank-till-anmalan" tabindex="-1"><a class="header-anchor" href="#lank-till-anmalan" aria-hidden="true">#</a> Länk till anmälan</h2>
 <p><a href="https://start.landslagetsfotbollsskola.se/sv/show_club/8701-horreds-if-ifk-oxnevalla" target="_blank" rel="noopener noreferrer">Anmäl dig här<ExternalLinkIcon/></a></p>
--- docs/anmalan.md
+++ docs/anmalan.md
@@ -4,6 +4,7 @@ title: Anmälan
 # Anmälan 
 Nu är anmälan till fotbollsskolan öppen!
 Registeringen sker via Svenska fotbollsförbundets hemsida och är enkel att gå igenom. 
+Observera att det finns ett begränsat antal platser, så anmäl dig redan idag!
 Om du har några frågor eller funderingar, [kontakta oss](kontakt)!
 
 ## Länk till anmälan
