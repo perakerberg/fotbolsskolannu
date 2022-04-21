3d040cdf01a6eafa91664a81a54b7b788b7697b6
Per Åkerberg <per.akerberg@live.se>
2022-04-21 13:59:40 +0200

    How to fix error tips

--- docs/.vuepress/.temp/pages/anmalan.html.vue
+++ docs/.vuepress/.temp/pages/anmalan.html.vue
@@ -4,5 +4,15 @@ Registeringen sker via Svenska fotbollsförbundets hemsida och är enkel att gå
 Det finns ett <em>begränsat antal platser</em>, så anmäl dig redan idag! ⚽ 🏃‍♀️ 🏃‍♂️</p>
 <p>Om du har några frågor eller funderingar, <a href="kontakt">kontakta oss</a>!</p>
 <h2 id="lank-till-anmalan" tabindex="-1"><a class="header-anchor" href="#lank-till-anmalan" aria-hidden="true">#</a> Länk till anmälan</h2>
-<p><a href="https://start.landslagetsfotbollsskola.se/sv/show_club/8701-horreds-if-ifk-oxnevalla" target="_blank" rel="noopener noreferrer">Anmäl dig här<ExternalLinkIcon/></a></p>
-</template>
+<p><a href="https://start.landslagetsfotbollsskola.se/sv/show_club/8701-horreds-if-ifk-oxnevalla" target="_blank" rel="noopener noreferrer">&lt;--Anmäl dig här--&gt;<ExternalLinkIcon/></a></p>
+<hr>
+<p><strong>Problem med anmälan på iPhone</strong></p>
+<p><em>Det kan vara problem att anmäla sig via iPhone från länken på Facebook. Följ stegen nedan för att lösa problemet.</em></p>
+<div style="display: flex; flex-direction:column;">
+    <div> Klicka på de tre punkterna längst ner till höger...</div>
+    <img style="max-width:400px" src="/images/browser-error.png"/>
+</div>
+<div style="display: flex; flex-direction:column;">
+...och välj "öppna i webbläsare".
+<img style="max-width:400px" src="/images/browser-fix.png"/>
+</div></template>
--- docs/anmalan.md
+++ docs/anmalan.md
@@ -10,4 +10,20 @@ Om du har några frågor eller funderingar, [kontakta oss](kontakt)!
 
 ## Länk till anmälan
 
-[Anmäl dig här](https://start.landslagetsfotbollsskola.se/sv/show_club/8701-horreds-if-ifk-oxnevalla)
+[<--Anmäl dig här-->](https://start.landslagetsfotbollsskola.se/sv/show_club/8701-horreds-if-ifk-oxnevalla)
+
+
+---
+**Problem med anmälan på iPhone**
+
+*Det kan vara problem att anmäla sig via iPhone från länken på Facebook. Följ stegen nedan för att lösa problemet.*
+
+<div style="display: flex; flex-direction:column;">
+    <div> Klicka på de tre punkterna längst ner till höger...</div>
+    <img style="max-width:400px" src="/images/browser-error.png"/>
+</div>
+<div style="display: flex; flex-direction:column;">
+...och välj "öppna i webbläsare".
+<img style="max-width:400px" src="/images/browser-fix.png"/>
+
+</div>
