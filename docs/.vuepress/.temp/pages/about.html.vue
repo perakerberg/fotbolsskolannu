eb044a59ae4704ce2a426898bee55f9f03c6235f
Per Åkerberg <per.akerberg@live.se>
2023-03-09 16:57:31 +0100

    Git ingore in effect

--- docs/.vuepress/.temp/pages/about.html.vue
+++
@@ -1,2 +0,0 @@
-<template><h1 id="" tabindex="-1"><a class="header-anchor" href="#" aria-hidden="true">#</a> </h1>
-</template>
--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><div style="display: flex; flex-direction: column; align-items:center; justify-content: center; padding: 1rem;">
-<a href="/start">
-<StartPage/>
-</a>
-</div>
-</div></template>
-
-
