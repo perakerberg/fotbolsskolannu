3f2448686a7f825ade23673dbb3e2eb4517f82d7
Per Åkerberg <per.akerberg@live.se>
2024-02-17 07:41:04 +0100

    Scalable

--- docs/.vuepress/components/LedarOMeter.vue
+++ docs/.vuepress/components/LedarOMeter.vue
@@ -44,10 +44,11 @@ export default {
 
 <style lang="scss" scoped>
 .LedarOMeter {
-  max-width: 1000px;
+  max-width: 100%;
   position: relative;
+  box-sizing: border-box;
   img {
-    width: 1000px;
+    width: 100%;
     height: auto;
     position: relative;
   }
@@ -55,19 +56,19 @@ export default {
   }
 
   .Ledare {
-    height: 42px;
-    width: 65px;
+    height: 42%;
+    width: 8%;
     position: absolute;
-    top: 26px;
-    left: 70px;
+
     opacity: 0.4;
     background-position: center;
-    background-size: cover;
+    background-size: contain;
+    background-repeat: no-repeat;
     background-image: url("https://res.cloudinary.com/dpd5cqqpz/image/upload/v1708092075/ledartroja_sbhjsk.png");
-    transform: rotate(0deg);
+    transform: rotate(0deg) translateX(10px);
 
     &--booked {
-      transform: rotate(8deg) scale(1.1);
+      transform: rotate(8deg) scale(1.1) translateX(10px);
       opacity: 1;
 
       transition: all 0.5s ease-in-out;
@@ -76,70 +77,70 @@ export default {
 
   @for $i from 1 through 10 {
     .Ledare1-#{$i} {
-      top: 50px;
-      left: -50px + ($i * 70px);
+      top: 18%;
+      left: ($i - 1) * 9.5%;
     }
   }
   @for $i from 1 through 10 {
     .Ledare2-#{$i} {
-      top: 120px;
-      left: -50px + ($i * 70px);
+      top: 50%;
+      left: ($i - 1) * 9.5%;
     }
   }
 }
 
-@media only screen and (max-width: 1000px) {
-  .LedarOMeter {
-    max-width: 500px;
-    position: relative;
-    img {
-      width: 500px;
-      max-width: 500px !important;
-    }
-    .Ledare {
-      height: 30px;
-      width: 40px;
-    }
+// @media only screen and (max-width: 1000px) {
+//   .LedarOMeter {
+//     max-width: 500px;
+//     position: relative;
+//     img {
+//       width: 500px;
+//       max-width: 500px !important;
+//     }
+//     .Ledare {
+//       height: 30px;
+//       width: 40px;
+//     }
 
-    @for $i from 1 through 10 {
-      .Ledare1-#{$i} {
-        top: 32px;
-        left: -30px + ($i * 46px);
-      }
-    }
-    @for $i from 1 through 10 {
-      .Ledare2-#{$i} {
-        top: 80px;
-        left: -30px + ($i * 46px);
-      }
-    }
-  }
-}
-@media only screen and (max-width: 499px) {
-  .LedarOMeter {
-    max-width: 300px;
-    position: relative;
-    img {
-      width: 300px;
-      max-width: 300px !important;
-    }
-    .Ledare {
-      height: 20px;
-      width: 25px;
-    }
+//     @for $i from 1 through 10 {
+//       .Ledare1-#{$i} {
+//         top: 32px;
+//         left: -30px + ($i * 46px);
+//       }
+//     }
+//     @for $i from 1 through 10 {
+//       .Ledare2-#{$i} {
+//         top: 80px;
+//         left: -30px + ($i * 46px);
+//       }
+//     }
+//   }
+// }
+// @media only screen and (max-width: 499px) {
+//   .LedarOMeter {
+//     max-width: 300px;
+//     position: relative;
+//     img {
+//       width: 300px;
+//       max-width: 300px !important;
+//     }
+//     .Ledare {
+//       height: 20px;
+//       width: 25px;
+//     }
 
-    @for $i from 1 through 10 {
-      .Ledare1-#{$i} {
-        top: 22px;
-        left: -20px + ($i * 28px);
-      }
-    }
-    @for $i from 1 through 10 {
-      .Ledare2-#{$i} {
-        top: 46px;
-        left: -20px + ($i * 28px);
-      }
-    }
-  }
-}
+//     @for $i from 1 through 10 {
+//       .Ledare1-#{$i} {
+//         top: 22px;
+//         left: -20px + ($i * 28px);
+//       }
+//     }
+//     @for $i from 1 through 10 {
+//       .Ledare2-#{$i} {
+//         top: 46px;
+//         left: -20px + ($i * 28px);
+//       }
+//     }
+//   }
+// }
 </style>
