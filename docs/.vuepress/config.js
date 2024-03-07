6e2e3b1cccd822a90a5504afcd3fba2c6290302e
Per Åkerberg <per.akerberg@live.se>
2024-03-07 20:18:16 +0100

    CLosed

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,10 +36,10 @@ export default defineUserConfig({
 
       { text: "Om", link: "/om" },
       // { text: "Ledare sökes!", link: "/ledare-sokes" },
-      {
-        text: "Anmälan",
-        link: "/anmalan",
-      },
+      // {
+      //   text: "Anmälan",
+      //   link: "/anmalan",
+      // },
       { text: "Kontakt", link: "/kontakt" },
     ],
     sidebar: false,
